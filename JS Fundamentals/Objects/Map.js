3bddde9119c68e49f83355d358eb5b27080af945
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-10-09 14:40:22 +0300

    up

---
+++ JS Fundamentals/Objects/Map.js
@@ -0,0 +1,13 @@
+let score = new Map();
+score.set("Peter",130);
+score.set("Maria",86);
+for(let [n,s] of score){
+    console.log(n + '->' + s);
+}
+let phonebook = new Map();
+phonebook.set("Stamat",120);
+phonebook.set("Sahil Jai",100);
+for(let [j,g] of phonebook){
+    console.log(j + '->' + g);
+}
+
