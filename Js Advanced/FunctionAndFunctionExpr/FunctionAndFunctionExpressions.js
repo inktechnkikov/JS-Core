66b2bf5ea58465e258b4cd8c1a7b3817f00a3d3a
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-10-16 18:39:21 +0300

    update

--- Js Advanced/FunctionAndFunctionExpr/FunctionAndFunctionExpressions.js
+++ Js Advanced/FunctionAndFunctionExpr/FunctionAndFunctionExpressions.js
@@ -1,1 +1,17 @@
+function max(arr) {
+    let maxValue = arr[0];
+    for (let a = 1; a < arr.length; a++) {
+        maxValue = Math.max(maxValue, arr[a]);
+    }
+    return maxValue;
+}
+    function min(arrMin) {
+        let minValue = arrMin[0];
+        for(let m = 1;m < arrMin.length;m++){
+            minValue = Math.min(minValue,arrMin[m]);
+        }
+        return minValue;
+    }
 
+console.log(max(['1','2','3','4','100']));
+console.log(min(['1','2','3','4','100']));
