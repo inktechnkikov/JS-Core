2cc333be55a442720c0951769446bb578e2bffe4
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-10-25 14:55:32 +0300

    up

---
+++ Js Advanced/Advanced Functions/FunctionProp.js
@@ -0,0 +1,16 @@
+function max(arr) {
+    console.log(max.length);
+    return arr;
+}
+(function () {
+    let num1 = 10;
+    let num2 = 5;
+    console.log(num1 + num2);
+})();
+max(['1','2']);
+(function product() {
+    let n = 5;
+    let n1 = 10;
+    let res = n * n1;
+    console.log(res);
+}());
---
+++ Js Advanced/Advanced Functions/iiFE.js
@@ -0,0 +1,6 @@
+let iife = function () {
+    console.log('im iife');
+    let n = 1.2;
+    let n2 = 0.56;
+    console.log(n * n2);
+}();
