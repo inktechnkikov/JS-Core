75e5c8cd718e98377d776ae894a6ba4f045d25e6
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-10-27 12:33:08 +0300

    up

---
+++ Js Advanced/Object Composition/Studebt.js
@@ -0,0 +1,8 @@
+let students = {
+    firstName:'Maria',
+    lastName:'Dimitrova',
+    age:22,
+    location:{zipcode:4150,city:'Plovdiv'}
+};
+console.log(students);
+console.log(students.location.city);
