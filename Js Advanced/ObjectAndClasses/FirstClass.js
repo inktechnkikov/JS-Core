cd8ff960fecdd4e657fc742902cb2989fce2bda2
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-10-31 20:17:15 +0200

    up

---
+++ Js Advanced/ObjectAndClasses/FirstClass.js
@@ -0,0 +1,9 @@
+class Rectangle {
+    constructor(width,height,color){
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+}
+let r = new Rectangle(20,30,'red');
+console.log(r);
---
+++ Js Advanced/ObjectAndClasses/classPerson.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name,age,weight,height,sex){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+}
+let person = new Person("Pesho",27,90,1.80);
+console.log(person);
