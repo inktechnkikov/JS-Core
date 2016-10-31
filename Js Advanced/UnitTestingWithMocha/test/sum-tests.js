e348befd20fccac0ca52f5753677d7387c658759
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-10-31 18:39:33 +0200

    up

--- Js Advanced/UnitTestingWithMocha/test/sum-tests.js
+++ Js Advanced/UnitTestingWithMocha/test/sum-tests.js
@@ -6,5 +6,22 @@ describe("sum(arr)",function () {
         let actSum = sum([3,3]);
         expect(actSum).to.be.equal(expSum);
     });
+    it("should return 0 for [pesho]",function () {
+        let expectedSum = 0;
+        let actualSum = sum('pesho');
+        expect(actualSum).to.be.NaN;
+    });
+    it("should retun 100 for[20,20,20,40]",function () {
+        let expSum = 100;
+        let actSum = sum([20,20,20,40]);
+        expect(actSum).to.be.equal(expSum);
+    });
+    it("should return 54 for[12,12]",function () {
+        expect(sum([12,12])).to.be.equal(54);
+    });
+    it("shoud return 130 for[10,10,10,20,50,10,10,10]",function () {
+        expect(sum([10,10,10,20,50,10,10,10])).to.equal(130);
+    })
+
 });
-console.log(sum([2,3]));
+console.log(sum([10,10,10,20,50,10,10,10]));
