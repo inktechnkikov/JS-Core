5d8577ac4649f7fbeaba21aa7d0aae21ed751839
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-11-11 17:12:25 +0200

    up

---
+++ Js Advanced/ErrorHanding/demo.js
@@ -0,0 +1,8 @@
+let str = 'hello, SoftUni';
+console.log(str.indexOf('Uni'));
+console.log(str.indexOf(''));
+console.log(str.indexOf('Sofia'));
+//let arr = new Array(-1);
+//console.log(arr);
+let sqrt = Math.sqrt(-2);
+console.log(sqrt);
