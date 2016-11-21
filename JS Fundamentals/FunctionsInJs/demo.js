87c17964583047c39df57f2e12520c09411f5d3b
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-11-21 22:08:49 +0200

    up

---
+++ JS Fundamentals/FunctionsInJs/demo.js
@@ -0,0 +1,15 @@
+function DoProcess(a,b) {
+
+   let result = a + b;
+   console.log(result);
+   return result;
+}
+DoProcess(10,5);
+
+let DoProcessMultiplication = function (b,c) {
+    let resultOfMultiplication = b * c;
+    console.log(resultOfMultiplication);
+    return resultOfMultiplication;
+};
+DoProcessMultiplication(5,5);
+
