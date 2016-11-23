19c978df2b84cf2ad31a10322af019a40f81260a
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-11-23 18:09:44 +0200

    changes uploaded

--- JS Applications/CreatingSPAwthoutFrameworks/BookLibrary/scripts/booklibraries.js
+++ JS Applications/CreatingSPAwthoutFrameworks/BookLibrary/scripts/booklibraries.js
@@ -1,3 +1,60 @@
 function startApplication() {
-    
+    sessionStorage.clear();
+
+    showHideMenuLinks();
+
+    showView('viewHome');
+
+    $('#linkHome').click(showHomeView);
+    $('#linkLogin').click(showLoginView);
+    $('#linkReg').click(showRegistrationView);
+    $('#linkListBook').click(listBooks);
+    $('#linkCreateBooks').click(showCreatedBooksView);
+    $('#linkLogout').click(logoutUser);
+
+    $('#btnLoginUser').click(loginUser());
+    $('#btnRegUser').click(registerUser);
+    $('#btnCreateBook').click(createBook);
+    $('#btnEditBook').click(editBook);
+
+    $("#errorBox, #infoBox, #loadingBox").click(function () {
+        $(this).fadeOut();
+    });
+
+    function showHideMenuLinks() {
+
+    }
+    function showView(viewName) {
+        
+    }
+    function showHomeView() {
+        showView('viewHome');
+    }
+    function loginUser() {
+        
+    }
+    function showLoginView() {
+
+    }
+    function showRegistrationView() {
+        
+    }
+    function listBooks() {
+        
+    }
+    function showCreatedBooksView() {
+        
+    }
+    function logoutUser() {
+        
+    }
+    function registerUser() {
+
+    }
+    function createBook() {
+        
+    }
+    function editBook() {
+
+    }
 }
