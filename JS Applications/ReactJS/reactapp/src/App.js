4b8f3c8d4a9e1e80d0ef86b61239ff5755cba7c9
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-11-26 16:17:27 +0200

    update

--- JS Applications/ReactJS/reactapp/src/App.js
+++ JS Applications/ReactJS/reactapp/src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Header from './Header';
 class App extends Component {
   render() {
     return (
       <div className="App">
+            <Header/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-            <div>
-                <p>This is reactJS</p>
-                <p>Hello ReactJSX</p>
-            </div>
         </p>
       </div>
     );
---
+++ JS Applications/ReactJS/reactapp/src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css';
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <a href="#">Home</a>
+                <a href="#">Login</a>
+                <a href="#">Register</a>
+            </div>
+        );
+    }
+}
+
+export default Header;
