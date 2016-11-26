7cc4f9952b7fffc55ffa4a945bfdf3c050a719db
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-11-26 15:41:17 +0200

    update

--- JS Applications/ReactJS/reactapp/src/App.js
+++ JS Applications/ReactJS/reactapp/src/App.js
@@ -6,12 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+            <div>
+                <p>This is reactJS</p>
+                <p>Hello ReactJSX</p>
+            </div>
         </p>
       </div>
     );
