80a8ef4b5f04f900c2ce881287e89ac01df7a0fe
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-11-26 16:30:53 +0200

    Footer added

--- JS Applications/ReactJS/reactapp/src/App.js
+++ JS Applications/ReactJS/reactapp/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
 class App extends Component {
   render() {
     return (
@@ -9,6 +10,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <Footer/>
       </div>
     );
   }
