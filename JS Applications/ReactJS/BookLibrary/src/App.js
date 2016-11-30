9233aef4aefc2b371426d8193f85e30b198e66fc
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-11-30 11:47:15 +0200

    ReactJs in progress

---
+++ JS Applications/ReactJS/BookLibrary/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import $ from 'jquery';
+import NavigationBar from './Components/NavigationBar';
+import Footer from './Components/Footer';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+        <NavigationBar/>
+          <div id="error-box">Error messgaes</div>
+          <div id="info-box">Info messages</div>
+          <div id="load-box">Load messges</div>x
+        </header>
+      <div id="main">
+        App view
+      </div>
+      <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ JS Applications/ReactJS/BookLibrary/src/Components/NavigationBar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './NavigationBar.css';
+
+export default class NavigationBar extends Component{
+    render(){
+        return(
+            <div className="navigation-bar">
+                im the navBar
+            </div>
+        );
+    }
+}
