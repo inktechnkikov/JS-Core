f55758593591fa15893128466e703612922cba38
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-11-30 20:04:03 +0200

    login form created

--- JS Applications/ReactJS/BookLibrary/src/Views/Login.js
+++ JS Applications/ReactJS/BookLibrary/src/Views/Login.js
@@ -1,7 +1,25 @@
-import React from 'react';
-export default function Login() {
-    return <div className="login-view">
-        <h1>Login</h1>
-        <p>Welcome to login</p>
-    </div>
+import React, {Component} from 'react';
+export default class Login extends Component{
+    render(){
+        return (
+            <form className="login-form" onSubmit={this.submitForm.bind(this)}>
+                <h1>Login</h1>
+                <label>
+                    <div>Username:</div>
+                    <input type="text" name="username" required ref={e => this.usernameField = e} />
+                </label>
+                <label>
+                    <div>Password:</div>
+                    <input type="password" name="password" required ref={e => this.passwordField = e} />
+                </label>
+                <div>
+                    <input type="submit" value="Login" />
+                </div>
+            </form>
+        );
+    }
+    submitForm(event){
+        event.preventDefault();
+        this.props.onsubmit(this.passwordField.value,this.usernameField.value);
+    }
 }
