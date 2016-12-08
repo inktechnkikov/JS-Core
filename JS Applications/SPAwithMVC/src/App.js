bbaad306e7b795e6b70d71d3654b52db47f2a064
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-08 16:07:59 +0200

    link to abut and catalog page available

--- JS Applications/SPAwithMVC/src/App.js
+++ JS Applications/SPAwithMVC/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Link} from 'react-router';
 
 class App extends Component {
   render() {
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <Link to="/catalog">Catalog</Link>
+
+          <Link to="/about">About</Link>
       </div>
     );
   }
