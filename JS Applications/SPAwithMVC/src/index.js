7be9da971fbb312faff80874517bc3273e874e9e
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-08 15:37:53 +0200

    catalog and about page added

--- JS Applications/SPAwithMVC/src/index.js
+++ JS Applications/SPAwithMVC/src/index.js
@@ -4,10 +4,21 @@ import App from './App';
 import './index.css';
 import {Router, Route, hashHistory} from 'react-router';
 
-
+let Catalog = React.createClass({
+    render:function () {
+       return <div>Catalog Page</div>;
+    }
+});
+let About = React.createClass({
+   render:function () {
+       return <div>About Page</div>;
+   }
+});
 ReactDOM.render(
  <Router history={hashHistory}>
-     <App/>
+   <Route path="/" component={App}/>
+     <Route path="catalog" component={Catalog}/>
+     <Route path="about" component={About}/>
  </Router>,
   document.getElementById('root')
 );
