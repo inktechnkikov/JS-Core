9abb13f2029be0cf9a8363ac0320e6234b7c1f5f
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-08 22:31:25 +0200

    home page added

--- JS Applications/SPAwithMVC/src/index.js
+++ JS Applications/SPAwithMVC/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import {Router, Route, hashHistory} from 'react-router';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 
 let Catalog = React.createClass({
     render:function () {
@@ -14,9 +14,15 @@ let About = React.createClass({
        return <div>About Page</div>;
    }
 });
+let Home = React.createClass({
+   render:function () {
+       return <div>Home Page</div>
+   }
+});
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
         <Route path="catalog" component={Catalog}/>
         <Route path="about" component={About}/>
             </Route>
