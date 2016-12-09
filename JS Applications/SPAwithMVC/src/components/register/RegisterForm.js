633d71fda81015c57805c0e5bbeb1f4507edf86c
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-10 00:37:33 +0200

    update Register form

--- JS Applications/SPAwithMVC/src/components/register/RegisterForm.js
+++ JS Applications/SPAwithMVC/src/components/register/RegisterForm.js
@@ -1,41 +1,19 @@
 import React, {Component} from 'react';
 
 class RegisterForm extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-          username:'',
-            password:'',
-            repeatpass:''
-        };
-        this.onChangeHandler = this.onChangeHandler.bind(this);
-        this.onSubmitHandler = this.onSubmitHandler.bind(this);
-    }
-    onChangeHandler(event) {
-        event.preventDefault();
-        let newState = {};
-        newState[event.target.name] = event.target.value;
-        this.setState(newState)
-    }
-    onSubmitHandler(event){
-        event.preventDefault();
-        alert('Submited');
-        console.log(this.state.username);
-        console.log(this.state.password);
-        console.log(this.state.repeatpass);
-    }
+
     render(){
         return(
-            <form onSubmit={this.onSubmitHandler}>
+            <form onSubmit={this.props.onSubmit}>
                 <div className="form-group">
                <label>
                    Username
                </label>
                 <input className="form-control"
-                        type="text"
+                       type="text"
                        name="username"
-                       value={this.state.username}
-                       onChange={this.onChangeHandler}/>
+                       value={this.props.username}
+                       onChange={this.props.onChange}/>
                 </div>
                 <div className="form-group">
                 <label>
@@ -44,8 +22,8 @@ class RegisterForm extends Component{
                 <input className="form-control"
                         type="password"
                        name="password"
-                       value={this.state.password}
-                       onChange={this.onChangeHandler}/>
+                       value={this.props.password}
+                       onChange={this.props.onChange}/>
                 </div>
                 <div className="form-group">
                 <label>
@@ -53,10 +31,11 @@ class RegisterForm extends Component{
                 </label>
                 <input className="form-control"
                         type="password"
-                       name="repeatpass" value={this.state.repeatpass}
-                       onChange={this.onChangeHandler}/>
+                       name="repeatpass"
+                       value={this.props.repeatpass}
+                       onChange={this.props.onChange}/>
                 </div>
-                <input type="submit" value="Register" />
+                <input type="submit" value="Register" className="btn btn-default"/>
             </form>
         );
     }
--- JS Applications/SPAwithMVC/src/components/register/RegisterPage.js
+++ JS Applications/SPAwithMVC/src/components/register/RegisterPage.js
@@ -1,11 +1,37 @@
 import React, {Component} from 'react';
 import RegisterForm from './RegisterForm';
 class RegisterPage extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password:'',
+            repeatpass:''
+        };
+        this.onChangeHandler=this.onChangeHandler.bind(this);
+        this.onSubmitHandler=this.onSubmitHandler.bind(this);
+    }
+    onChangeHandler(event) {
+        event.preventDefault();
+        let newState = {};
+        newState[event.target.name] = event.target.value;
+        this.setState(newState)
+    }
+    onSubmitHandler(event){
+        event.preventDefault();
+
+    }
     render(){
         return(
             <div>
                 <h1>Register Page</h1>
-                <RegisterForm/>
+                <RegisterForm
+                    username={this.state.username}
+                    password={this.state.password}
+                    repeatpass={this.state.repeatpass}
+                    onChange={this.onChangeHandler}
+                    onSubmit={this.onSubmitHandler}
+                />
             </div>
         );
     }
