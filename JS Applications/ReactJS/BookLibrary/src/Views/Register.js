3532e93320ca8dfda409f6917eb8dea6057174a2
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-10 22:40:50 +0200

    up

--- JS Applications/ReactJS/BookLibrary/src/Views/Register.js
+++ JS Applications/ReactJS/BookLibrary/src/Views/Register.js
@@ -1,6 +1,10 @@
-import React from 'react'
-export default function Register() {
-    return <div className="register-view">
-        <h1>Welcome to Register</h1>
-    </div>
+import React,{Component} from 'react';
+class Register extends  Component {
+    render() {
+        return (
+            <div className="register-view">
+            <h1>Welcome to Register</h1>
+        </div>
+        );
+    }
 }
