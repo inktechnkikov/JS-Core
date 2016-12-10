1280d0cb317bbe4933b56b46e9da25a2cdccf2a9
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-10 16:52:47 +0200

    change view

--- JS Applications/SPAwithMVC/src/App.js
+++ JS Applications/SPAwithMVC/src/App.js
@@ -5,49 +5,64 @@ import observer from './models/observer';
 import Greeting from './components/common/Greeting';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      loggedIn:false,
-        username:''
-    };
-    this.onSessionUpdate=this.onSessionUpdate.bind(this);
-  }
-  componentDidMount(){
-    observer.onSessionUpdate = this.onSessionUpdate;
-    this.checkUserCredentials();
-  }
-  onSessionUpdate(){
-    console.log('check credentials');
-  }
-  checkUserCredentials(){
-    let username = sessionStorage.getItem('username');
-    if(!username){
-      this.setState({
-        loggedIn:false
-      })
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false,
+            username: ''
+        };
+        this.onSessionUpdate = this.onSessionUpdate.bind(this);
     }
-    else{
-      this.setState({
-        loggedIn:true,
-          username:username
-      })
+    componentDidMount() {
+        observer.onSessionUpdate = this.onSessionUpdate;
+        this.checkUserCredentials();
     }
-  }
-  render() {
-    return (
-      <div className="container">
-        <Header>
-          <Greeting username={this.state.username} loggedIn={this.state.loggedIn}/>
-          <Link to="/" className="btn btn-default">Home</Link>
-          <Link to="/catalog" className="btn btn-default">Catalog</Link>
-          <Link to="/about" className="btn btn-default">About</Link>
-          <Link to="/register" className="btn btn-default">Register</Link>
-        </Header>
-          {this.props.children}
-      </div>
-    );
-  }
-}
+    onSessionUpdate() {
+        this.checkUserCredentials();
+    }
+    checkUserCredentials() {
+        let username = sessionStorage.getItem('username');
+        if (!username) {
+            this.setState({
+                loggedIn: false
+            })
+        }
+        else {
+            this.setState({
+                loggedIn: true,
+                username: username
+            })
+        }
+    }
 
+    render() {
+        if (this.state.loggedIn) {
+            return (
+                <div className="container">
+                  <Header>
+                    <Link to="/" className="btn btn-default">Home</Link>
+                    <Link to="/catalog" className="btn btn-default">Catalog</Link>
+                    <Link to="/about" className="btn btn-default">About</Link>
+                    <Link to="/logout" className="btn btn-default">Logout</Link>
+                    <Greeting username={this.state.username} loggedIn={this.state.loggedIn}/>
+                  </Header>
+                    {this.props.children}
+                </div>
+            )
+        }
+        else {
+            return (
+                <div className="container">
+                  <Header>
+                    <Link to="/" className="btn btn-default">Home</Link>
+                    <Link to="/about" className="btn btn-default">About</Link>
+                    <Link to="/register" className="btn btn-default">Register</Link>
+                    <Greeting username={this.state.username} loggedIn={this.state.loggedIn}/>
+                  </Header>
+                    {this.props.children}
+                </div>
+            );
+        }
+    }
+}
 export default App;
